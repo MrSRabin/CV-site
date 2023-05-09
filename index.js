79f34026a0b228837168082a5c3b31a519f7aa7a
MrSRabin <132851076+MrSRabin@users.noreply.github.com>
2023-05-09 00:12:41 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function dropdown(a) {
-    /* document.getElementById("dropdowncontent1").classList.toggle("show"); */
     a.parentNode.getElementsByClassName('dropdown-content')[0].classList.toggle("show");
     var arrow = a.querySelector('.arrow');
     if (arrow.innerHTML === "▲") {
@@ -8,3 +7,18 @@ function dropdown(a) {
         arrow.innerHTML = "▲";
     }
 }
+
+document.querySelector("#submit1").addEventListener("click", function(){
+    let correctAnswer = "shaul rabin"
+    let answer = document.querySelector("#q1").value.toLowerCase();
+    if (answer == correctAnswer){
+        document.querySelector("#q1").style.backgroundColor = "Green";
+        document.querySelector("#q1result").innerHTML = "Correct";
+        document.querySelector("#q1result").style.visibility = "visible";
+    }
+    else {
+        document.querySelector("#q1").style.backgroundColor = "Red";
+        document.querySelector("#q1result").innerHTML = "Incorrect";
+        document.querySelector("#q1result").style.visibility = "visible";
+    }
+});
